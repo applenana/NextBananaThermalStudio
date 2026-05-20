b41c6f753af920f5a2012f770d50fa8f90c15c85
applenana <83851967+applenana@users.noreply.github.com>
2026-05-20 23:51:54 +0800

    fix: parallax offset direction and remove parallaxEnabled gate in render pipeline

--- lib/render/render_pipeline.dart
+++ lib/render/render_pipeline.dart
@@ -132,8 +132,8 @@ RenderedFrame renderPipeline({
       vw: visibleW,
       vh: visibleH,
       params: params.fusion,
-      parallaxDx: params.parallaxEnabled ? params.parallaxDx : 0.0,
-      parallaxDy: params.parallaxEnabled ? params.parallaxDy : 0.0,
+      parallaxDx: params.parallaxDx,
+      parallaxDy: params.parallaxDy,
     );
   }
 
